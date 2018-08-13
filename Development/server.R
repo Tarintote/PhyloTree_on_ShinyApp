3901e9f3d564d2f4763209f32f4039f1400615d0
ieuser <ieuser@ieuser-no-MacBook-Pro-3.local>
2018-08-13 21:45:26 +0900

    add#Install_Pkg shell scirpt

--- Development/server.R
+++ Development/server.R
@@ -5,14 +5,20 @@ if(!require("foreach", character.only=TRUE)){
   install.packages("foreach")
 }
 
-pkgs.name = c("shiny", "devtools", "reticulate", "phangorn", "phytools", "phylocanvas",
+pkgs.name = c("RNeXML", "shiny", "devtools", "reticulate", "phangorn", "phytools",
  "igraph", "ggplot2", "dendextend", "RColorBrewer", "colormap", "gpclib", "maptools", "leaflet",
  "rgl")
 
 foreach(i = pkgs.name, .packages="foreach") %do%{
     if(!require(i, character.only=TRUE)){
       install.packages(i)
+      require(i, character.only=TRUE)
     }
+}
+
+if(!require("phylocanvas", character.only=TRUE)){
+  devtools::install_github("zachcp/phylocanvas")
+  require("phylocanvas", character.only=TRUE)
 }
 
 gc <- import("generate_controler")
