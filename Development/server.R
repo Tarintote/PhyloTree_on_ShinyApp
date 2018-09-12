0460b36414f7162ed901da9f0ab40ed2625e03ea
ieuser <ieuser@ieuser-no-MacBook-Pro-3.local>
2018-09-12 19:24:41 +0900

    Rのパッケージをinstallするタイミングを変えました

--- Development/server.R
+++ Development/server.R
@@ -1,25 +1,9 @@
 options(rgl.useNULL=TRUE)
 #options(rgl.printRglwidget=FALSE)
 
-if(!require("foreach", character.only=TRUE)){
-  install.packages("foreach")
-}
-
 pkgs.name = c("RNeXML", "shiny", "devtools", "reticulate", "phangorn", "phytools",
  "igraph", "ggplot2", "dendextend", "RColorBrewer", "colormap", "gpclib", "maptools", "leaflet",
  "rgl")
-
-foreach(i = pkgs.name, .packages="foreach") %do%{
-    if(!require(i, character.only=TRUE)){
-      install.packages(i)
-      require(i, character.only=TRUE)
-    }
-}
-
-if(!require("phylocanvas", character.only=TRUE)){
-  devtools::install_github("zachcp/phylocanvas")
-  require("phylocanvas", character.only=TRUE)
-}
 
 gc <- import("generate_controler")
 nex <- import("helper")
